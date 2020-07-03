f722b1cd208592100172ce7dbfa3a9a29a8bafbe
lachezar.balev <lachezar.balev@gmail.com>
2020-07-03 21:39:37 +0300

    Initial commit.

--- security/src/main/java/bg/softuni/security/config/SecurityConfig.java
+++ security/src/main/java/bg/softuni/security/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package bg.softuni.security.config;
 import bg.softuni.security.user.DemoUserDetailsService;
 import javax.sql.DataSource;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
