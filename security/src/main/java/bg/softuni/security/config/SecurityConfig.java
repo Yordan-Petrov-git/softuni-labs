07efc73bcd24838be7a74716a65bf25d2d7744a2
lachezar.balev <lachezar.balev@gmail.com>
2020-07-03 19:43:30 +0300

    jdbc authentication

--- security/src/main/java/bg/softuni/security/config/SecurityConfig.java
+++ security/src/main/java/bg/softuni/security/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package bg.softuni.security.config;
 
+import javax.sql.DataSource;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -9,16 +10,22 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-  final PasswordEncoder passwordEncoder;
+  private final PasswordEncoder passwordEncoder;
+  private final DataSource dataSource;
 
-  public SecurityConfig(PasswordEncoder passwordEncoder) {
+  public SecurityConfig(PasswordEncoder passwordEncoder,
+      DataSource dataSource) {
     this.passwordEncoder = passwordEncoder;
+    this.dataSource = dataSource;
   }
 
   @Override
   protected void configure(AuthenticationManagerBuilder auth) throws Exception {
     auth.
-        inMemoryAuthentication().
+        jdbcAuthentication().
+        withDefaultSchema().
+        dataSource(dataSource).
+        passwordEncoder(passwordEncoder).
         withUser("user").password(passwordEncoder.encode("user")).roles("USER").and().
         withUser("admin").password(passwordEncoder.encode("admin")).roles("ADMIN", "USER");
   }
@@ -27,10 +34,14 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
   protected void configure(HttpSecurity http) throws Exception {
     http.
         authorizeRequests().
-        antMatchers("/home").permitAll().
+        antMatchers("/h2_console/**", "/home").permitAll().
         antMatchers("/admin").hasRole("ADMIN").
         antMatchers("/user").hasRole("USER").
         and().
         formLogin();
+
+    //h2 magic, TODO: delete me
+    http.csrf().disable();
+    http.headers().frameOptions().disable();
   }
 }
