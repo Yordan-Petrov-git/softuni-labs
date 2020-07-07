d3831bbdb6f28d93deb1d7e8ceb19f10b4610287
lachezar.balev <lachezar.balev@gmail.com>
2020-07-07 20:07:14 +0300

    Intermediate impl.

---
+++ hateoas/src/main/java/bg/softuni/hateoas/web/CoursesController.java
@@ -0,0 +1,48 @@
+package bg.softuni.hateoas.web;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
+import bg.softuni.hateoas.model.Course;
+import bg.softuni.hateoas.repository.CourseRepository;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.Link;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/courses")
+public class CoursesController {
+
+  private final CourseRepository courseRepository;
+
+  public CoursesController(CourseRepository courseRepository) {
+    this.courseRepository = courseRepository;
+  }
+
+  @GetMapping
+  public ResponseEntity<CollectionModel<EntityModel<Course>>> getAllCourses() {
+    List<EntityModel<Course>> courses =courseRepository.
+        findAll().
+        stream().
+        map(course -> EntityModel.of(course, createCourseLinks(course))).
+        collect(Collectors.toList());
+
+    return ResponseEntity.ok(
+        CollectionModel.of(courses,
+            linkTo(methodOn(CoursesController.class).getAllCourses()).withSelfRel()));
+  }
+
+  private Link[] createCourseLinks(Course course) {
+    List<Link> result = new ArrayList<>();
+    // TODO
+    return result.toArray(new Link[0]);
+  }
+}
