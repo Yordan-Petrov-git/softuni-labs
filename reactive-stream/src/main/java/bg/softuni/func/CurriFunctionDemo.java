8fc384e814ca85ac2ad342b5e8458f4e70a1bca7
lachezar.balev <lachezar.balev@gmail.com>
2020-07-31 20:09:25 +0300

    curring

---
+++ reactive-stream/src/main/java/bg/softuni/func/CurriFunctionDemo.java
@@ -0,0 +1,18 @@
+package bg.softuni.func;
+
+import java.util.function.Function;
+
+public class CurriFunctionDemo {
+
+  public int multiply(int a, int b) {
+    return a * b;
+  }
+
+  Function<Integer, Function<Integer, Integer>> currMulti = u -> v -> u * v;
+
+  public void test() {
+    int res1 = multiply(2, 3);
+    int res2 = currMulti.apply(2).apply(3);
+  }
+
+}
