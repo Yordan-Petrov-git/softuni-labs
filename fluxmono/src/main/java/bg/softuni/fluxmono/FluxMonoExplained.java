110e77a1cc98ef3250076899985b8ab0bb44eccd
lachezar.balev <lachezar.balev@gmail.com>
2020-07-31 20:19:46 +0300

    Flux mono simple demo

---
+++ fluxmono/src/main/java/bg/softuni/fluxmono/FluxMonoExplained.java
@@ -0,0 +1,68 @@
+package bg.softuni.fluxmono;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.Random;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+public class FluxMonoExplained {
+
+  private User user1 = new User().setFirstName("A").setLastName("B");
+  private User user2 = new User().setFirstName("A1").setLastName("B1");
+
+  public Flux<User> getAllUsersNew() {
+    return Flux.just(user1, user2);
+  }
+
+  public List<User> getAllUsers() {
+    return List.of(user1, user2);
+  }
+
+  public Mono<User> getUserReactive() {
+    if (isAuthenticated())
+      return Mono.just(user1);
+    else
+      return Mono.empty();
+  }
+
+  public User getUser() {
+    if (isAuthenticated())
+      return user1;
+    else
+      return null;
+  }
+
+  public Optional<User> getUserOpt() {
+    if (isAuthenticated())
+      return Optional.of(user1);
+    else
+      return Optional.empty();
+  }
+
+  private boolean isAuthenticated() {
+    return new Random().nextBoolean();
+  }
+}
+
+class User {
+  private String firstName, lastName;
+
+  public String getLastName() {
+    return lastName;
+  }
+
+  public User setLastName(String lastName) {
+    this.lastName = lastName;
+    return this;
+  }
+
+  public String getFirstName() {
+    return firstName;
+  }
+
+  public User setFirstName(String firstName) {
+    this.firstName = firstName;
+    return this;
+  }
+}
