86270fe7ee9d14663caddd4dd87da5b9e623648c
lachezar.balev <lachezar.balev@gmail.com>
2020-07-31 21:04:46 +0300

    New webflux service

---
+++ webflux-service/src/main/java/bg/softuni/webfluxservice/WebfluxServiceApplication.java
@@ -0,0 +1,13 @@
+package bg.softuni.webfluxservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebfluxServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebfluxServiceApplication.class, args);
+	}
+
+}
