043d5c72937ab3bb5118dce91e6faf544ab09cd4
lachezar.balev <lachezar.balev@gmail.com>
2020-08-11 14:32:41 +0300

    Actuator demo.

---
+++ actuate/src/main/java/bg/softuni/actuate/ActuateApplication.java
@@ -0,0 +1,13 @@
+package bg.softuni.actuate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActuateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActuateApplication.class, args);
+	}
+
+}
---
+++ actuate/src/test/java/bg/softuni/actuate/ActuateApplicationTests.java
@@ -0,0 +1,13 @@
+package bg.softuni.actuate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActuateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
